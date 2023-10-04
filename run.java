0cf97324723a62f5c3e04c4a30d7953948caa7ec
Grigory Shatalin <grigory.shatalin@akvelon.com>
2023-10-04 16:21:07 -0700

    Aggregation passing test cases and commented out

--- run.java
+++ run.java
@@ -1,11 +1,9 @@
 public class run {
     public static void main(String[] args){
-        Fraction f1 = new Fraction(3, 6, 11);
-       Fraction f2 = new Fraction(-1, 1, 2);
-       Fraction f3 = new Fraction(0, 3);
-
-        System.out.println(f1);
-        System.out.println(f2);
-        System.out.println(f3);
-    }
+        Fraction f2 = new Fraction(3, 4);   
+        Rectangle[] array2 = {new Rectangle(10, 30), new Rectangle(11, 22), new Rectangle(7, 2)};
+        Aggregation obj2 = new Aggregation(f2, array2, 3.4);
+        array2[1].setWidth(100);// changing the original Rectangle object in array
+        System.out.println(obj2.toString());
+    }  
 }
